884572d34cb45a12629d2692af31ef0607059f5f
Adarsh Powar <164239665+BusyEngineer01@users.noreply.github.com>
2025-07-25 19:50:22 +0530

    some resource files added

--- app/src/main/java/com/techad/healtylifeapp/MainActivity.kt
+++ app/src/main/java/com/techad/healtylifeapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.techad.healtylifeapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
--- app/src/main/java/com/techad/healtylifeapp/SplashScreen.kt
+++ app/src/main/java/com/techad/healtylifeapp/SplashScreen.kt
@@ -1,6 +1,8 @@
 package com.techad.healtylifeapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,11 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+        }, 3000)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
